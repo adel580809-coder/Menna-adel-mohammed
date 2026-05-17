34daec33f9d45044234bf0f8e407e11c3890f81b
Meena Adel Mohammed <adel580809@gmail.com>
2026-05-17 08:26:41 +0300

    Add files via upload

---
+++ script (3).js
@@ -0,0 +1,21 @@
+function appendValue(value) {
+    
+    document.getElementById("result").value += value;
+}
+
+function clearResult() {
+    document.getElementById("result").value = "";
+}
+
+function calculate() {
+    let x = document.getElementById("result").value;
+    
+    try {
+    
+        if (x) {
+            document.getElementById("result").value = eval(x);
+        }
+    } catch (error) {
+        document.getElementById("result").value = "Error";
+    }
+}
